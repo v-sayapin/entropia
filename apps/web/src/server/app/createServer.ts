1961120a46f19b98b5d4ce01f3d1910daa8378d3
Vladislav Sayapin <70110788+v-sayapin@users.noreply.github.com>
2026-03-29 14:09:13 +0300

    prune dev server code in the build stage

--- apps/web/src/server/app/createServer.ts
+++ apps/web/src/server/app/createServer.ts
@@ -2,7 +2,6 @@ import Fastify from 'fastify';
 
 import { staticPlugin } from 'src/server/app/plugins/static';
 import { getSsrProcessor } from 'src/server/app/ssr';
-import { isProduction } from 'src/server/utils/env';
 
 export const createServer = async () => {
 	const app = Fastify({
@@ -10,7 +9,7 @@ export const createServer = async () => {
 		exposeHeadRoutes: false,
 	});
 
-	if (isProduction) {
+	if (process.env.NODE_ENV === 'production') {
 		await app.register(staticPlugin);
 	} else {
 		const { vitePlugin } = await import('src/server/app/plugins/vite');
