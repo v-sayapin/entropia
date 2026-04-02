93cd8c77f205b0c73a09638b3b1440107632c152
Vladislav Sayapin <70110788+v-sayapin@users.noreply.github.com>
2026-04-02 13:34:28 +0300

    graceful shutdown

--- apps/web/src/server/app/index.ts
+++ apps/web/src/server/app/index.ts
@@ -1,8 +1,22 @@
+import type { FastifyInstance } from 'fastify';
+
 import { createServer } from 'server/app/createServer';
 import { host, port } from 'server/app/env';
 
+const shutdown = async (app: FastifyInstance, reason: string): Promise<void> => {
+	app.log.info({ reason }, 'Server is shutting down');
+	try {
+		await app.close();
+	} finally {
+		process.exit(0);
+	}
+};
+
 const start = async (): Promise<void> => {
 	const app = await createServer();
+
+	process.on('SIGTERM', () => shutdown(app, 'SIGTERM'));
+	process.on('SIGINT', () => shutdown(app, 'SIGINT'));
 
 	try {
 		await app.listen({ host, port });
