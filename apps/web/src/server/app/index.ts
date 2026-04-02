8d0574059e317d1e2ddeb2ab00656e98a313956e
Vladislav Sayapin <70110788+v-sayapin@users.noreply.github.com>
2026-04-02 13:34:28 +0300

    graceful shutdown

--- apps/web/src/server/app/index.ts
+++ apps/web/src/server/app/index.ts
@@ -4,6 +4,18 @@ import { host, port } from 'src/server/utils/env';
 const start = async () => {
 	const app = await createServer();
 
+	const shutdown = async (reason: string) => {
+		app.log.info({ reason }, 'Server is shutting down');
+		try {
+			await app.close();
+		} finally {
+			process.exit(0);
+		}
+	};
+
+	process.on('SIGTERM', () => void shutdown('SIGTERM'));
+	process.on('SIGINT', () => void shutdown('SIGINT'));
+
 	try {
 		await app.listen({ host, port });
 	} catch (error) {
