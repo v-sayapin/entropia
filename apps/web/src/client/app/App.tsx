856061111845464b5215751d5297bb6f07e39313
Vladislav Sayapin <70110788+v-sayapin@users.noreply.github.com>
2026-04-08 21:57:32 +0300

    delete hello world client

--- apps/web/src/client/app/App.tsx
+++ apps/web/src/client/app/App.tsx
@@ -4,10 +4,6 @@ import type { Component } from 'solid-js';
 
 import type { AppProps } from 'shared/app/types';
 
-import { Content } from 'client/components/Content';
-
-import './App.css';
-
 export const App: Component<AppProps> = (_props) => {
-	return <Content />;
+	return <>entropia app</>;
 };
